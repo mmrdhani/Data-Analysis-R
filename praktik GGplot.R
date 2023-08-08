31903c94dddb172888457fce0851fff4d78d78e6
Muhammad Ramdhani <125638569+mmrdhani@users.noreply.github.com>
2023-08-08 12:42:28 +0700

    Delete praktik GGplot.R

--- praktik GGplot.R
+++
@@ -1,5 +0,0 @@
-install.packages('datasauRus')
-
-library('datasauRus') 
-
-ggplot(datasaurus_dozen,aes(x=x,y=y,colour=dataset))+geom_point()+theme_void()+theme(legend.position = "none")+facet_wrap(~dataset,ncol=3) 
